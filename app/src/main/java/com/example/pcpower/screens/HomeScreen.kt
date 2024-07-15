509f1bcd3a2cf8a93447dc33641c4df234940f94
superbobby2000 <coudercleo10@gmail.com>
2024-07-15 09:22:10 -0400

    clean imports

--- app/src/main/java/com/example/pcpower/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pcpower/screens/HomeScreen.kt
@@ -22,10 +22,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.AlertDialog
